bd3fdb0cfc8c8cb6784281217add9aa2bd1e5b9c
nick pg <nick@nick.nick>
2021-03-01 21:46:35 -0500

    (library) better versioning; v2.0.0-ALPHA-DEV

--- contentswitcher-v2.js
+++ contentswitcher-v2.js
@@ -8,82 +8,91 @@
  *   strict: true, false, or undefined (=false). If true, a javascript error will be thrown when a nonexistent contentSet code or content ID is used. If not true, the undefined value will be returned when a nonexistent contentSet code or content ID is used.
  * @return {*} A contentswitcher instance configured with the given options.
  */
-window.contentSwitcherV2 = function(contentSetCodes, options) {
-    var obj = {};
-    obj.selected = contentSetCodes[0];
-    obj.contentSetCodes = contentSetCodes;
-    options = options || {};
-    if (typeof(options) === 'string') {
-        var mode = options;
-        options = {mode: mode};
-    }
-    obj.mode = options.mode;
-    obj.stringsById = {};
-
-    var fnSelectContentSet = function(contentSetCode) {
-        obj.selected = contentSetCode;
-        // NO VALIDATION YET!
-        // need something like: if contentSetCode not in obj.contentSetCodes then throw error
-    }
-
-    function ContentErrorMessage(contentSetCode, content) {
-        var contentIdText = 'content';
-        if (typeof(content) === 'string') {
-            contentIdText = 'content ID "' + content + '"';
-        }
-        else if (typeof(content) === 'object') {
-            contentIdText = 'content with codes ' + Object.keys(content).map(x => '"'+x+'"').join(', ')
-        }
-        return `Content missing for code ${contentSetCode} in ${contentIdText}`;
-    }
-
-    var fnGetImmediate = function(getArg) {
-        var value = undefined;
-        if (typeof(getArg) == "string") {
-            // by id
-            if (obj.stringsById.hasOwnProperty(getArg) && obj.stringsById[getArg].hasOwnProperty(obj.selected)) {
-                value = obj.stringsById[getArg][obj.selected];
-            }
-            else if (options.strict) {
-                throw new Error(ContentErrorMessage(obj.selected, getArg));
-            }
-        }
-        else if (typeof(getArg) == "object") {
-            // via supplied text
-            if (getArg.hasOwnProperty(obj.selected)) {
-                value = getArg[obj.selected];
-            }
-            else if (options.strict) {
-                throw new Error(ContentErrorMessage(obj.selected, getArg));
-            }
-        }
-        return value;
+window.contentSwitcherV2 = (function(){
+    var extraProperties = {
+        version: '2.0.0-ALPHA-DEV'
     };
 
-    var fnGetDefer = function(getArg) {
-        var fn = function() {
-            return fnGetImmediate(getArg);
+    var constructorFn = function(contentSetCodes, options) {
+        var obj = {};
+        obj.selected = contentSetCodes[0];
+        obj.contentSetCodes = contentSetCodes;
+        options = options || {};
+        if (typeof(options) === 'string') {
+            var mode = options;
+            options = {mode: mode};
+        }
+        obj.mode = options.mode;
+        obj.stringsById = {};
+
+        var fnSelectContentSet = function(contentSetCode) {
+            obj.selected = contentSetCode;
+            // NO VALIDATION YET!
+            // need something like: if contentSetCode not in obj.contentSetCodes then throw error
+        }
+
+        function ContentErrorMessage(contentSetCode, content) {
+            var contentIdText = 'content';
+            if (typeof(content) === 'string') {
+                contentIdText = 'content ID "' + content + '"';
+            }
+            else if (typeof(content) === 'object') {
+                contentIdText = 'content with codes ' + Object.keys(content).map(x => '"'+x+'"').join(', ')
+            }
+            return `Content missing for code ${contentSetCode} in ${contentIdText}`;
+        }
+
+        var fnGetImmediate = function(getArg) {
+            var value = undefined;
+            if (typeof(getArg) == "string") {
+                // by id
+                if (obj.stringsById.hasOwnProperty(getArg) && obj.stringsById[getArg].hasOwnProperty(obj.selected)) {
+                    value = obj.stringsById[getArg][obj.selected];
+                }
+                else if (options.strict) {
+                    throw new Error(ContentErrorMessage(obj.selected, getArg));
+                }
+            }
+            else if (typeof(getArg) == "object") {
+                // via supplied text
+                if (getArg.hasOwnProperty(obj.selected)) {
+                    value = getArg[obj.selected];
+                }
+                else if (options.strict) {
+                    throw new Error(ContentErrorMessage(obj.selected, getArg));
+                }
+            }
+            return value;
         };
-        return fn;
-    }
 
-    var fnGetByMode = function(getArg) {
-        if (obj.mode == "defer") {
-            return fnGetDefer(getArg);
-        } else {
-            return fnGetImmediate(getArg)
+        var fnGetDefer = function(getArg) {
+            var fn = function() {
+                return fnGetImmediate(getArg);
+            };
+            return fn;
         }
-    }
 
-    var fnAdd = function(id, values) {
-        obj.stringsById[id] = values;
-        // NO VALIDATION YET!
+        var fnGetByMode = function(getArg) {
+            if (obj.mode == "defer") {
+                return fnGetDefer(getArg);
+            } else {
+                return fnGetImmediate(getArg)
+            }
+        }
+
+        var fnAdd = function(id, values) {
+            obj.stringsById[id] = values;
+            // NO VALIDATION YET!
+        };
+
+        var objPublic = fnGetByMode;
+        objPublic.switch = fnSelectContentSet;
+        objPublic.defer = fnGetDefer;
+        objPublic.get = fnGetImmediate;
+        objPublic.add = fnAdd;
+        return objPublic;
     };
 
-    var objPublic = fnGetByMode;
-    objPublic.switch = fnSelectContentSet;
-    objPublic.defer = fnGetDefer;
-    objPublic.get = fnGetImmediate;
-    objPublic.add = fnAdd;
-    return objPublic;
-};
+    Object.assign(constructorFn, extraProperties);
+    return constructorFn;
+})();
